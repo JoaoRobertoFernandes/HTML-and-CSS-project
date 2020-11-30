6f3f48dea2483a530cec3ac659d09d819d733e74
JoaoFernandes <joaofernandes4891@gmail.com>
2020-11-30 13:43:11 -0300

    Doing the parallax effect.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-//abre e fecha menu
+$(function () {
+
+//Menu open and close
 $('.nav-toggle, .nav-close').click(function (e) {
     e.preventDefault();
     $('.nav').toggleClass('active');
   });
+
+//Header fixer 
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 100){
+    $('.header').addClass('fixed');
+  }else {
+    $('.header').removeClass('fixed');
+  }
+});
+});
